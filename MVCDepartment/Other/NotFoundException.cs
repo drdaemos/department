75a99cbe23624f74caf441dfa41cd45fdf739237
Тимур <wtispy@gmail.com>
2013-11-29 20:25:31 +0400

    Попытки смастерить интерфейс под парсер и его простенькую какбыРеализацию

---
+++ MVCDepartment/Other/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDepartment.Other
+{
+    public class NotFoundException : Exception
+    {
+    }
+}
---
+++ MVCDepartment/Other/Parser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO; 
+
+namespace MVCDepartment.Other
+{
+    interface IParser
+    {
+        string getInfo(String query); 
+    }
+
+    public class Parser
+    {
+        string filePath;
+        string file; 
+        public Parser(string filePath)
+        {        
+            if (filePath == null)
+            {
+                throw new NullReferenceException("File is null"); 
+            }
+            file = System.IO.File.ReadAllText(filePath);
+            this.filePath = filePath; 
+        }
+
+        public string getInfo(String query)
+        {
+            if ((query == null) || (query == ""))
+            {
+                throw new ArgumentException("Queue is empty"); 
+            }
+            String retString;
+
+
+
+            throw new NotFoundException(); 
+        }
+    }
+
+}
